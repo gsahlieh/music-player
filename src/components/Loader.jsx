b97616538c2a2e402e3360a366b041d04655cc1c
gsah3992 <gsah3992@uni.sydney.edu.au>
2022-10-25 17:36:29 +1100

    Updated Version

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = ({title}) => {
+  return (
+    <div className="loading w-full flex justify-center items-center flex-col">
+        <span>Loading</span>
+    </div>
+  )
+}
+
+export default Loader
